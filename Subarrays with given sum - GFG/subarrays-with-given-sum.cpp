09621629a246ecee707ac748bc8eed76a84d3613
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2023-08-14 11:51:23 +0530

    Added solution - LeetHub

---
+++ Subarrays with given sum - GFG/subarrays-with-given-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int subArraySum(int nums[], int n, int k)
+    {
+    	unorderedmap<int,int>map;
+        map[0]=1;
+
+        int preSum=0,cnt=0;
+
+        for(int i=0;i<n;i++){
+            preSum+=nums[i];
+
+            int diff=preSum-k;
+            cnt+=map[diff];
+            map[preSum]+=1;
+        }
+
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,sum=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    cin>>sum;
+	    Solution ob;
+	    cout<<ob.subArraySum(arr, n, sum);
+	    cout<<'\n';
+	}
+	return 0;
+}
+// } Driver Code Ends
