a986f0b95d749b98ce982a026c63333ef549ecd0
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2023-08-26 09:36:14 +0530

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        
+        Node*even=new Node(0);
+        Node*p1=even;
+        Node*odd=new Node(0);
+        Node*p2=odd;
+        
+        Node*ptr=head;
+        while(ptr){
+            if(ptr->data % 2==0){
+                even->next=ptr;
+                even=even->next;
+                ptr=ptr->next;
+            }
+            else{
+                odd->next=ptr;
+                odd=odd->next;
+                ptr=ptr->next;
+            }
+        }
+        even->next=p2->next;
+        odd->next=NULL;
+        return p1->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
