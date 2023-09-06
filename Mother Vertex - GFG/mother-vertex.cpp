0253fb83888ab5c2bb99dd62f3d4a92548a367df
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2023-09-06 08:25:12 +0530

    Added solution - LeetHub

--- Mother Vertex - GFG/mother-vertex.cpp
+++ Mother Vertex - GFG/mother-vertex.cpp
@@ -21,21 +21,33 @@ class Solution
 	int findMotherVertex(int V, vector<int>adj[])
 	{
 	    //Was able to pass 1213/1223 testcases on my own approach
+	    //Then referred solution
+	    
+	    //First of all create ans var and visited vector
 	    int ans=0;
 	    vector<int>vis(V,0);
 	    
+	    //Apply dfs calls for each vertex if its unvisited
 	    for(int i=0;i<V;i++){
+	        //This will provide us with vertex who was lastly called for dfs
 	        if(vis[i]==0){
+	            //Store latest dfs call vertex
 	            ans=i;
 	            dfs(i,adj,vis);
 	        }
 	    }
+	    //Again initialize the visited vertex with all zeros
 	    vis=vector<int>(V,0);
+	    //Again have a dfs call
 	    dfs(ans,adj,vis);
+	    //Now check for visited array , if found any element 
+	    //unvisited then its not possible to visit all nodes 
+	    //from a vertex
 	    for(auto n:vis){
 	        if(n==0)return -1;
 	    }
-	    
+	    //Else return the last vertex for which dfs call was done as 
+	    //all vertices are visited in its dfs call
 	    return ans;
 	}
 
