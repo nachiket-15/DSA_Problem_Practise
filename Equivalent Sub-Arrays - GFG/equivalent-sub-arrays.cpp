086405bffee74ac3c3d9d43c7b51773c145eaa9c
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2023-12-08 09:04:37 +0530

    Added solution - LeetHub

--- Equivalent Sub-Arrays - GFG/equivalent-sub-arrays.cpp
+++ Equivalent Sub-Arrays - GFG/equivalent-sub-arrays.cpp
@@ -6,45 +6,36 @@ using namespace std;
 class Solution
 {
 public:
-    
-    
     int countDistinctSubarray(int arr[], int n)
     {
         unordered_map<int,int>freq;
-        
+        int ans_cnt=0;
         set<int>st;
-        for(int i=0;i<n;i++)
-            st.insert(arr[i]);
-            
-        //Get the number of distinct elements 
-        int k=st.size();
-        int ptr1=0,ptr2=0,cnt=0;
+        for(int i=0;i<n;i++)st.insert(arr[i]);
         
-        while(ptr2<n){
-            freq[arr[ptr2]]++;
-            //Ask yourself why here while loop and not if condition
-            
-            //As we will potentially miss out on subarrays due to 
-            //duplicate elements and we will eventually exhaust outer 
-            //loop as ptr will get increment 
-            while(k==freq.size()){
-                //Increment with all possible subarrays till end
-                cnt+=n-ptr2;
-                //Remove element from start so as to slide window
-                freq[arr[ptr1]]--;
-                if(freq[arr[ptr1]]==0){
-                    freq.erase(arr[ptr1]);
+        int k=st.size();
+        
+        //Two pointer approach needs to be utilized
+        int i=0,j=0;
+        
+        //We will use j for traversing 
+        //i for cutting subarrays from front for next iteration
+        while(j<n){
+            freq[arr[j]]++;
+            while(freq.size()==k){
+                ans_cnt+=n-j;
+                freq[arr[i]]--;
+                
+                if(freq[arr[i]]==0){
+                    freq.erase(arr[i]);
                 }
-                ptr1++;
+                
+                i++;
             }
-            ptr2++;
+            j++;
         }
-        //for that see second test case 
-        //There if in dry run u will notice that ptr1 takes only first
-        //two elemets of array eventually missing last 3 elemets taking into subarrays 
-        //individual subarrays 
-        //In that case u miss 2 subarrays 4,2,4 and 2,4 from last 
-        return cnt;
+        
+        return ans_cnt;
     }
 };
 
