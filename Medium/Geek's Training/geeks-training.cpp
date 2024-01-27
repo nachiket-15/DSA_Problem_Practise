c1342b8353a5752d5baa399fbeb3b236c8518383
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-01-27 11:09:48 +0530

    Updated Solution - GfG to GitHub

--- Medium/Geek's Training/geeks-training.cpp
+++ Medium/Geek's Training/geeks-training.cpp
@@ -6,28 +6,31 @@ using namespace std;
 class Solution {
   public:
     int f(int day,int last,vector<vector<int>>&dp,vector<vector<int>> &points)
-{
-    //BASE CASE
-    if(day==0){
+    {
+        //BASE CASE
+        if(day==0){
+            int maxi=0;
+            for(int i=0;i<=2;i++){
+                if(i!=last){
+                    maxi=max(points[0][i],maxi);}
+            }
+            return dp[day][last]=maxi;
+        }
+        
+        if(dp[day][last]!=-1){
+            return dp[day][last];
+        }
+        
         int maxi=0;
+        
         for(int i=0;i<=2;i++){
-            if(i!=last){
-                maxi=max(points[0][i],maxi);}
+            if (i != last) {
+                int activity = points[day][i] + f(day - 1, i, dp, points);
+                maxi = max(activity, maxi);
+            }
         }
         return dp[day][last]=maxi;
     }
-    if(dp[day][last]!=-1){
-        return dp[day][last];
-    }
-    int maxi=0;
-    for(int i=0;i<=2;i++){
-        if (i != last) {
-            int activity = points[day][i] + f(day - 1, i, dp, points);
-            maxi = max(activity, maxi);
-        }
-    }
-    return dp[day][last]=maxi;
-}
     int maximumPoints(vector<vector<int>>& points, int n) {
         vector<vector<int>>dp(n,vector<int>(4,-1));
         //Reason behind creating a dp of n*4 instead of n*3 is that
