3f9fdcb48e6e942df0ebaf74095572d189d7e872
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-01-27 10:28:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum money/maximum-money.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximizeMoney(int N , int K) {
+        // code here
+        if(N%2){
+            N=N/2;
+            N+=1;
+        }
+        else{
+            N=N/2;
+        }
+        
+        return N*K;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+
+        cin>>N>>K;
+
+        Solution ob;
+        cout << ob.maximizeMoney(N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
