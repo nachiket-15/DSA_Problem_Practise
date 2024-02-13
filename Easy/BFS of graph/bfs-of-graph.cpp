7026e80f7133ff3cda374b2340d47958e08595a0
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-02-13 21:55:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -9,24 +9,33 @@ class Solution {
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         vector<int>ans;
         queue<int>q;
+        
         vector<int>vis(V,0);
+        
         q.push(0);
         vis[0]=1;
-        while(!q.empty()){
+        
+        while(!q.empty())
+        {
             int node=q.front();
             q.pop();
+            
             ans.push_back(node);
             
             for(auto it:adj[node]){
+                
                 if(!vis[it]){
                     q.push(it);
                     vis[it]=1;
                 }
+                
             }
         }
+        
         return ans;
     }
 };
+
 
 
 //{ Driver Code Starts.
