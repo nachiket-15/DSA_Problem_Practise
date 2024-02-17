9e4bd400eb25b9335877ac5ab36be765eac95399
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-02-17 07:04:32 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find the median/find-the-median.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    // Code here.
+		    // Sort the array
+        sort(v.begin(), v.end());
+
+        int n = v.size();
+
+        // Calculate the median based on odd or even number of elements
+        if (n % 2 == 1) {
+            return v[n / 2];
+        } else {
+            return (v[(n - 1) / 2] + v[n / 2]) / 2;
+        }
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
