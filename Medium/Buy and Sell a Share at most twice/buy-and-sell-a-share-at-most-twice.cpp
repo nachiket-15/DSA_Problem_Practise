5c071d457737e80cbec575e47549ebb490f455fa
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-02-23 21:52:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
@@ -5,25 +5,27 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-int solve(int ind,int buy,int cap,vector<int>& prices,int n,vector<vector<vector<int>>>&dp)
+class Solution
 {
-    if(ind==n ||cap==0)return 0;
-    if(dp[ind][buy][cap]!=-1)return dp[ind][buy][cap];
-    if(buy==1){
-        //buy or not buy
-        return dp[ind][buy][cap]=max(-prices[ind]+solve(ind+1,0,cap,prices,n,dp),0+solve(ind+1,1,cap,prices,n,dp));
-    }
-    //Sell or dont sell 
-    return dp[ind][buy][cap]=max(prices[ind]+solve(ind+1,1,cap-1,prices,n,dp),0+solve(ind+1,0,cap,prices,n,dp));
-}
-int maxProfit(vector<int>&prices){
-    //Write your code here..
-    int n=prices.size();
-    vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-    return solve(0,1,2,prices,n,dp);
-}
+    public:
+        //User function Template for C++
+        
+        int maxProfit(vector<int>&prices){
+            //previosuly done using DP
+            int buy1 = INT_MIN, sell1 = 0, buy2 = INT_MIN, sell2 = 0;
+        
+            for(int price : prices) 
+            {
+                buy1 = max(buy1, -price);
+                sell1 = max(sell1, buy1 + price);
+                buy2 = max(buy2, sell1 - price);
+                sell2 = max(sell2, buy2 + price);
+            }
+        
+            return sell2;
+        }
+};
 
 //{ Driver Code Starts.
 
@@ -36,7 +38,8 @@ int main(){
         cin>>n;
         vector<int> price(n);
         for(int i=0;i<n;i++) cin>>price[i];
-        cout<<maxProfit(price)<<endl;
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
     }
 
 }
