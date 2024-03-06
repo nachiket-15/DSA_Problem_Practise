791ed1ac0e6b19dbcb59a735a796e2dd1deb280a
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-03-06 09:24:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
+++ Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -9,18 +9,46 @@ class Solution
     public:
         vector <int> search(string pattern, string text)
         {
-            
-            int m=pattern.size();
-            vector<int>ans;
-            for(int i=0;i<=text.size()-pattern.size();i++)
-            {
-                string str=text.substr(i,m);
-                if(str==pattern){
-                    ans.push_back(i+1);
-                }
+            //code here.
+            vector<int> indices;
+
+    // Prime number used for hashing
+    int q = 101;
+
+    int m = pattern.size();
+    int n = text.size();
+    int i, j, hash_p = 0, hash_t = 0, h = 1, d = m + n;
+
+    // Calculate h: (d^(m-1)) % q
+    for (i = 0; i < m - 1; i++) {
+        h = (h * d) % q;
+    }
+
+    // Calculate hash value for pattern and text
+    for (i = 0; i < m; i++) {
+        hash_p = (pattern[i] + d * hash_p) % q;
+        hash_t = (text[i] + d * hash_t) % q;
+    }
+
+    // Find the match
+    for (i = 0; i <= n - m; i++) {
+        // Check the hash values of current windows of text and pattern.
+        if (hash_p == hash_t) {
+            for (j = 0; j < m; j++) {
+                if (text[i + j] != pattern[j]) break;
             }
-            
-            return ans;
+            // if pattern == text
+            if (j == m) indices.push_back(i+1);
+        }
+        // Calculate the hash value for next window : adding trailing digit and remove leading digit
+        if (i < n - m) {
+            hash_t = (d * (hash_t - text[i] * h) + text[i + m]) % q;
+            // Converting negative value of hash_t to positive
+            if (hash_t < 0) hash_t = (hash_t + q);
+        }
+    }
+
+    return indices;
         }
      
 };
