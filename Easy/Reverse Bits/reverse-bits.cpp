3b78b4e20eddc826b95d3460cc8b5988009d30f3
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-04-13 08:29:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        // code here
+        
+        //ESE viva 
+        long long ans = 0; 
+        int pow = 31; 
+        while(x > 0) {
+            if(x & 1)
+                ans = ans + (1LL << pow);
+            x >>= 1;
+            pow--; 
+        }
+        return ans; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
