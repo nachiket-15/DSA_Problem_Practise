4c919013541405cc57ef1142ef9718fdf13c3fea
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-05-25 11:29:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
+++ Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.cpp
@@ -11,33 +11,34 @@ class Solution{
     //Function to return a list containing the union of the two arrays. 
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
-         vector<int>ans;
-        
-        for(int i=0;i<n;i++){
-            ans.push_back(arr1[i]);
-        }
-        
-        for(int i=0;i<m;i++){
-            ans.push_back(arr2[i]);
-        }
-        
-        int i=0;
-        
-        sort(ans.begin(),ans.end());
-        for(int j=1;j<m+n;j++){
-            if(ans[j]!=ans[i]){
-                i++;
-            }
-            ans[i]=ans[j];
-        }
-        
-        int decide=(m+n)-i-1;
-        // cout<<decide;
-        for(int i=0;i<decide;i++){
-            ans.pop_back();
-        }
-        
-        return ans;
+        int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
     }
 };
 
