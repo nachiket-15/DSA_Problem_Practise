e10498aabd20d8d8ded9f649710ede5a2e59d8d4
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-06-18 00:03:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -31,10 +31,30 @@ public:
 	{
 	    //This problem uses pick/nonpick strategy
 	    
-	    vector<int>dp(n,-1);
-	    return solve(arr,n-1,dp);
+	    vector<int>dp(n,0);
+	   // return solve(arr,n-1,dp);
+	   
+	    // Base case: If there are no elements in the array, return 0
+        dp[0] = arr[0];
+        
+        // Iterate through the elements of the array
+        for (int i = 1; i < n; i++) {
+            // Calculate the maximum value by either picking the current element
+            // or not picking it (i.e., taking the maximum of dp[i-2] + arr[i] and dp[i-1])
+            int pick = arr[i];
+            if (i > 1)
+                pick += dp[i - 2];
+            int nonPick = dp[i - 1];
+            
+            // Store the maximum value in the dp array
+            dp[i] = max(pick, nonPick);
+        }
+        
+        // The last element of the dp array will contain the maximum sum
+        return dp[n - 1];
 	}
 };
+
 
 
 //{ Driver Code Starts.
