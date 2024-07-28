e13bbc6026246166936bd5e2ab5463e5c7c5757f
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-07-28 09:32:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        map<char,int>map;
+        string ans="";
+        
+        for(char ch:str)
+        {
+            if(map.find(ch)!=map.end())
+            {
+                continue;
+            }else{
+                map[ch]=1;
+                ans+=ch;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
