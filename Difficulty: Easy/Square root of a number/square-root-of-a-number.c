669ca84c9ce9a22cf74523decb152e0e9495ba83
Nachiket Deshmukh <115225266+nachiket-15@users.noreply.github.com>
2024-08-13 09:56:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.c
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+#include <stdio.h>
+
+
+// } Driver Code Ends
+//User function Template for C
+#define ll long long
+long long int floorSqrt(long long int x) 
+{
+    //Today done on own
+      ll low=0;
+      ll high=x;
+      ll ans=0;
+      while(low<=high){
+          ll mid=(low+high)/2;
+          
+          if(mid*mid<=x){
+              ans=mid;
+              low=mid+1;
+          }else{
+              high=mid-1;
+          }
+      }
+      
+    return ans;
+}
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        long long n;
+        scanf("%ld", &n);
+
+        printf("%ld\n", floorSqrt(n));
+    }
+    return 0;
+}
+
+// } Driver Code Ends
